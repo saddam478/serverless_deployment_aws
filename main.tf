97f3952b56b980eb59cc05c59e1b7c2cda3cfe6e
saddam478 <shaik.saddamaws@gmail.com>
2025-02-25 09:31:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-unique-name"  # Change this to your bucket name
-    key            = "terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
-  }
-}
-
 provider "aws" {
   region = "ap-south-1"
 }
