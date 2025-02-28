1dc75779498280413d549bebc9f1fb4b0ef9c9f6
saddam478 <shaik.saddamaws@gmail.com>
2025-02-28 12:13:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -255,9 +255,9 @@ output "user_pool_client_id" {
   value = aws_cognito_user_pool_client.user_pool_client.id
 }
 
-output "cognito_domain_url" {
-  value = "https://${aws_cognito_user_pool_domain.cognito_domain.domain}.auth.ap-south-1.amazoncognito.com"
-}
+#output "cognito_domain_url" {
+#  value = "https://${aws_cognito_user_pool_domain.cognito_domain.domain}.auth.ap-south-1.amazoncognito.com"
+#}
 
 
 output "invoke_url" {
