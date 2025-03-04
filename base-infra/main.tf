3a05b44022ddd7e883b1092e5eb35ca342dc0117
saddam478 <shaik.saddamaws@gmail.com>
2025-03-04 12:50:53 +0530

    Update main.tf

--- base-infra/main.tf
+++ base-infra/main.tf
@@ -1,12 +1,12 @@
-#terraform {
-#  backend "s3" {
-#    bucket         = "terraform-state-bucket-unique-name123"
-#    key            = "terraform.tfstate"
-#    region         = "ap-south-1"
-#    dynamodb_table = "terraform-locks"
-#    encrypt        = true
-#  }
-#}
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-unique-name123"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
 provider "aws" {
   region = "ap-south-1"
 }
